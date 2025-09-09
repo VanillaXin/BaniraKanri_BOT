a51e67d05e57bea21cbe5c8b9d211e1ddafb65ea
Tsuki <TsukiMaaii@gmail.com>
2025-09-09 16:28:17 +0800

    feat 1.添加PlantCode

---
+++ src/main/java/xin/vanilla/banira/coder/message/PlantCode.java
@@ -0,0 +1,63 @@
+package xin.vanilla.banira.coder.message;
+
+import org.springframework.stereotype.Component;
+import xin.vanilla.banira.coder.common.BaniraCode;
+import xin.vanilla.banira.coder.common.MessageCoder;
+import xin.vanilla.banira.domain.BaniraCodeContext;
+import xin.vanilla.banira.enums.EnumCodeType;
+import xin.vanilla.banira.util.BaniraUtils;
+import xin.vanilla.banira.util.CollectionUtils;
+import xin.vanilla.banira.util.PlantCipher;
+
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 花言草语
+ */
+@Component
+public class PlantCode implements MessageCoder {
+
+    @Override
+    public List<String> getExample() {
+        return List.of(
+                CODE_START + CollectionUtils.getRandomElement(types) + CODE_END
+        );
+    }
+
+    @Override
+    public String getName() {
+        return "花言草语";
+    }
+
+    @Override
+    public String getDesc() {
+        return "将消息内容进行植物编码";
+    }
+
+    @Override
+    public int getPriority() {
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public EnumCodeType getType() {
+        return EnumCodeType.MSG;
+    }
+
+    private static final Set<String> types = BaniraUtils.mutableSetOf(
+            "plant", "plantcoding", "花言草语", "胡言乱语"
+    );
+
+    @Override
+    public boolean match(String msg) {
+        return types.contains(msg);
+    }
+
+    @Override
+    public BaniraCodeContext execute(BaniraCodeContext context, BaniraCode code, String placeholder) {
+        if (notMatch(code)) return context;
+        return context.msg(PlantCipher.encode(context.msg()));
+    }
+
+}
